963d8fdcb363d1edaab1bba335e13310da5d6a0f
Sergiy Koliada <rhombl4@gmail.com>
2019-03-30 01:15:53 +0200

    refactor service

--- app/services/import_csv_file.rb
+++ app/services/import_csv_file.rb
@@ -2,7 +2,6 @@
 
 # Service object for CSV-import
 class ImportCSVFile
-  require 'CSV'
   ROW_MAP = {
     0 => :name,
     1 => :photo_url,
@@ -12,7 +11,7 @@ class ImportCSVFile
     5 => :producer
   }.freeze
 
-  def self.process(file)
+  def self.call(file)
     import_service = new(file)
     import_service.import
     if import_service.success?
@@ -73,6 +72,72 @@ class ImportCSVFile
 
   private
 
+  def process_data(data, rows_count:)
+    # TODO: refactor this int oa separate service
+    data.each_with_object([]).with_index do |(source_row, arry), i|
+      row = source_row.dup
+
+      next if detect_wrong_column_count(row, rows_count, i)
+      next if detect_empty_column(row, i)
+      coerse_row_values(row)
+      next if detect_falsy_coersed_values(row, source_row, i)
+
+      product_hash = row.each_with_object({}).with_index do |(c, hsh), j|
+        hsh[ROW_MAP[j]] = c
+      end
+
+      arry << product_hash
+      # TODO: change the flow to saving by batches. this will reduce
+      # memory usage for large CSVs
+      # save_batch if arry.size > N
+      # clean_arry
+    end
+  end
+
+  def detect_wrong_column_count(row, rows_count, number)
+    return false if row.size == rows_count
+    add_error(row: number, error: 'Bad row', data: row)
+    true
+  end
+
+  def detect_empty_column(row, number)
+    return false if row.none?(&:blank?)
+    add_error(row: number, error: 'Empty column', data: row)
+    true
+  end
+
+  def coerse_row_values(row)
+    row[ROW_MAP.key(:sku)]     = downcased_string(row[ROW_MAP.key(:sku)])
+    row[ROW_MAP.key(:barcode)] = positive_integer(row[ROW_MAP.key(:barcode)])
+    row[ROW_MAP.key(:price)]   = positive_integer(row[ROW_MAP.key(:price)])
+  end
+
+  def detect_falsy_coersed_values(row, source_row, number)
+    faulty_column = row.find_index(&:blank?)
+    return false if faulty_column.blank?
+    col = ROW_MAP[faulty_column]
+    add_error(row: number, data: source_row, error: "Faulty value at #{col}")
+    true
+  end
+
+  def positive_integer(val)
+    integer = Integer(val)
+    integer.negative? ? nil : integer
+  rescue StandardError
+    nil
+  end
+
+  def downcased_string(val)
+    val.downcase
+  rescue StandardError
+    nil
+  end
+
+  def add_error(row:, error:, data:)
+    error_hash = { row: row, error: error, data: data }
+    row_errors << error_hash
+  end
+
   def errors
     return @errors if defined? @errors
     @errors = [].tap do |array|
@@ -82,47 +147,8 @@ class ImportCSVFile
 
   def source
     return @source if defined? @source
-    @source = CSVSource.open @source_file
+    @source = CSVSource.call(@source_file)
     errors.add('Source file not found') if @source.blank?
     @source
-  end
-
-  def process_data(data, rows_count:)
-    # TODO: refactor this int oa separate service
-    data.each_with_object([]).with_index do |(source_row, arry), i|
-      row = source_row.dup
-      if row.size != rows_count
-        row_errors << { row: i, error: 'Bad row', data: source_row}
-        next
-      end
-
-      if row.any?(&:blank?)
-        row_errors << { row: i, error: 'Empty column', data: source_row}
-        next
-      end
-
-      sku = row[ROW_MAP.key(:sku)]
-      row[ROW_MAP.key(:sku)] = (sku.downcase rescue nil)
-
-      barcode = row[ROW_MAP.key(:barcode)]
-      row[ROW_MAP.key(:barcode)] = (Integer(barcode) rescue nil)
-
-      price = row[ROW_MAP.key(:price)]
-      row[ROW_MAP.key(:price)] = (Integer(price) rescue nil)
-
-      if faulty_column = row.find_index(&:blank?)
-        row_errors << { row: i, error: "Faulty value at #{ROW_MAP[faulty_column]}", data: source_row }
-        next
-      end
-
-      product_hash = row.each_with_object({}).with_index do |(c, hsh), j|
-        hsh[ROW_MAP[j]] = c
-      end
-
-      arry << product_hash
-      # TODO: change the flow to saving by batches. this will reduce memory usage for large CSVs
-      # save_batch if arry.size > N
-      # clean_arry
-    end
   end
 end
