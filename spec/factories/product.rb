e47d8b1c50978c697777f6df75d8359d21e389b4
Sergiy Koliada <rhombl4@gmail.com>
2019-03-30 01:17:56 +0200

    Configure factory bot

--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -6,5 +6,17 @@ FactoryBot.define do
     price       { FFaker::Random.rand(1_000_000) }
     sku         { FFaker::SSN.ssn }
     producer    { FFaker::Company.name }
+
+    trait :invalid_price do
+      price { -FFaker::Random.rand(10) }
+    end
+
+    trait :invalid_ean do
+      barcode { -FFaker::Random.rand(10) }
+    end
+
+    trait :invalid_column do
+      name { '' }
+    end
   end
 end
